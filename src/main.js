c528b5a0c8934cfd117e9bb362088410e376ae40
prestontewell <preston@tewellphoto.com>
2019-06-25 15:48:53 -0500

    add function and buttons for filter, add separate function for pagination. refactor.

--- src/main.js
+++ src/main.js
@@ -10,13 +10,6 @@ import Vue2Filters from 'vue2-filters'
 axios.defaults.baseURL =
 	process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/'
 
-// const instance = axios.create({
-//   httpsAgent: new https.Agent({  
-//     rejectUnauthorized: false
-//   })
-// });
-// instance.get('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json');
-
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home" id="quotes">
-    <!-- <button v-on:click="previousPage">Previous</button>
-    <button v-on:click="nextPage">Next</button> -->
     <div class="pageNav">
       <a v-for="i in numOfPages" :key="i" v-on:click="changePage(i)"> {{ i }} </a>
-    </div>
+    </div><br>
+    <div class="filter">
+      <label><input type="radio" v-model="selectedTheme" value="All" /> All </label>
+      <label><input type="radio" v-model="selectedTheme" value="movies" /> Movies </label>
+      <label><input type="radio" v-model="selectedTheme" value="games" /> Games </label>
+    </div><br>
     <div class="form-group">
-      <label for='quote'> Search By Quote</label>
-      <input type="text" v-model="themeFilter">
-    </div>
-    <div v-for= "quote in filterBy(totalQuotes, themeFilter, 'quote')">
+      <label for='quote'> Search By Quote Per Page </label>
+      <input type="text" v-model="quoteFilter">
+    </div><br>
+    <div v-for= "quote in filterBy(splicedQuotes, quoteFilter, 'quote')">
       <h1>"{{ quote.quote }}"</h1>
       <div class="source">
         <h2> - {{ quote.source}}</h2>
@@ -42,98 +45,92 @@ import Vue2Filters from "vue2-filters"
 
 export default {
   mixins: [Vue2Filters.mixin],
-  
+
   data: function() {
     return {
       quotes: [
-        {"source":"The Wolf","context":"Pulp Fiction","quote":"That's thirty minutes away. I'll be there in ten.","theme":"movies"},
-        {"quote":"The first rule of Fight Club is: you do not talk about Fight Club. The second rule of Fight Club is: you DO NOT talk about Fight Club.","context":"Fight Club","source":"Tyler Durden", "theme":"movies"},
-        {"quote":"See that thing on my belt? Grab it! Whatever you do, don’t let go.","context":"Batman", "source":"Batman","theme":"movies"},
-        {"quote":"My momma always said, ‘Life was like a box of chocolates. You never know what you’re gonna get.", "context":"Forrest Gump", "source":"Forrest Gump", "theme":"movies"},
-        {"quote":"Ray, next time someone asks you if you’re a god, you say YES!","context":"Ghostbusters","source":"Winston Zeddemore","theme":"movies"},
-        {"quote":"They’ve done studies, you know. Sixty percent of the time, it works every time.","context":"Anchorman","source":"Brian Fantana","theme":"movies"},
-        {"quote":"This is so bad it’s gone past good and back to bad again.","context":"Ghost World","source":"Enid","theme":"movies"},
-        {"quote":"Go that way, really fast. If something gets in your way, turn.","context":"Better Off Dead","source":"Charles De Mar","theme":"movies"},
-        {"quote":"Put. That coffee. Down. Coffee's for closers only.","context":"Glengarry Glen Ross","source":"Blake","theme":"movies"},
-        {"quote":"Good? Bad? I'm the guy with the gun.","context":"Army of Darkness","source":"Ash","theme":"movies"},
-        {"quote":"Are you gonna bark all day, little doggy, or are you gonna bite?","context":"Reservoir Dogs","source":"Mr. Blonde","theme":"movies"},
-        {"quote":"There are two types of people in the world - those with a gun, and those who dig. Now dig!","context":"The Good, the Bad, and the Ugly","source":"Blondie","theme":"movies"},
-        {"quote":"If you try to run, I've got six little friends and they can all run faster than you can.","context":"From Dusk Til Dawn","source":"Seth","theme":"movies"},
-        {"quote":"Are you gonna do something? Or just stand there and bleed?","context":"Tombstone","source":"Wyatt Earp","theme":"movies"},
-        {"quote":"I love the smell of napalm in the morning","context":"Apocalypse now","source":"Kilgore","theme":"movies"},
-        {"quote":"I could be the walrus. I'd still have to bum rides off people","context":"Ferris Bueller's Day Off","source":"Ferris","theme":"movies"},
-        {"quote":"You either die a hero, or live long enough to see yourself become the villain.","context":"The Dark Knight","source":"Harvey Dent","theme":"movies"},
-        {"quote":"If you hold back anything, I'll kill ya. If you bend the truth or I think you're bending the truth, I'll kill ya. If you forget anything, I'll kill ya. In fact, you're gonna have to work very hard to stay alive, Nick. Now, do you understand everything I've just said? 'Cause if you don't, I'll kill ya! Now, Mr Bubble and Squeak, you may enlighten me.","context":"Lock, Stock and Two Smoking Barrels","source":"Rory Breaker","theme":"movies"},
-        {"quote":"You should never underestimate the predictability of stupidity.","context":"Snatch","source":"Bullet Tooth Tony","theme":"movies"},
-        {"quote":"In the quiet words of the Virgin Mary... come again? ","context":"Snatch","source":"Brick Top","theme":"movies"},
-        {"quote":"Heavy is good, heavy is reliable. If it doesn't work you can always hit them with it. ","context":"Snatch","source":"Boris 'The Blade' Yurinov","theme":"movies"},
-        {"quote":"Oh, I'm sorry, did I break your concentration? ","context":"Pulp Fiction","source":"Jules","theme":"movies"},
-        {"quote": "Momentum, a function of mass and velocity, is conserved between portals. In layman's terms, speedy thing goes in, speedy thing comes out.","context":"Portal","source":"GlaDOS","theme":"games"},
-        {"quote": "You talkin' to me?","context": "Taxi Driver","source": "Travis Bickle","theme": "movies"},
-        {"quote": "Are you not entertained! Are you not entertained! Is this not why you are here!","context": "Gladiator","source": "Maximus","theme": "movies"},
-        {"quote": "Here's Johnny","context": "Jack Torrance","source": "The Shining","theme": "movies"},
-        {"quote": "Mama always said life was like a box of chocolates. You never know what you're gonna get.","context": "Forest Gump","source": "Forest Gump","theme": "movies"},
-        {"quote": "E.T. phone home","context": "E.T.","source": "E.T. The Extra-Terrestrial","theme": "movies"},
-        {"quote": "Toto, I've got a feeling we're not in Kansas anymore","context": "Dorothy","source": "The Wizard of Oz","theme": "movies"},
-        {"quote":"Open the pod bay doors, please, HAL","context":"2001: A Space Odyssey","source":"Dave","theme":"movies"},
-        {"quote":"I'm not bad. I'm just drawn that way.", "source":"Jessica Rabbit", "context":"Who Framed Roger Rabbit", "theme":"movies"},
-        {"quote":"When I first saw you, I thought you were handsome. Then, of course, you spoke.", "source":"Carol", "context":"As Good As It Gets", "theme":"movies"},
-        {"quote":"Listen, here's the thing. If you can't spot the sucker in your first half hour at the table, then you ARE the sucker.", "source":"Mike", "context":"Rounders", "theme":"movies"},
-        {"quote":"The cake is a lie","context":"Portal","source":"Corpse","theme":"games"},
-        {"quote":"Welcome, everybody, to Monday Night Combat! We'd like to remind fans that we're committed to creating a safe, comfortable, enjoyable experience for only our Club-level and VIP guests. Everybody else...pay up for better seats next time!", "context":"Monday Night Combat", "source": "Mickey", "theme":"games"},
-        {"quote":"Good men mean well. We just don't always end up doing well", "context":"Dead Space 3", "source": "Isaac", "theme":"games"},
-        {"quote":"The next time you raise a hand to me will be the last time you have hands", "context":"Game of Thrones", "source":"Khaleesi","theme":"movies"},
-        {"quote":"I'm not in the meth business. I'm in the empire business", "context":"Breaking Bad", "source":"Walter","theme":"movies"},
-        {"quote":"If you don't like what's being said, change the conversation", "context":"Mad Men", "source":"Don Draper","theme":"movies"}
+        {"source":"The Wolf","context":"Pulp Fiction","quote":"That's thirty minutes away. I'll be there in ten.", "theme":"movies"},
+        {"quote":"The first rule of Fight Club is: you do not talk about Fight Club. The second rule of Fight Club is: you DO NOT talk about Fight Club.","context":"Fight Club","source":"Tyler Durden",  "theme":"movies"},
+        {"quote":"See that thing on my belt? Grab it! Whatever you do, don’t let go.","context":"Batman", "source":"Batman", "theme":"movies"},
+        {"quote":"My momma always said, ‘Life was like a box of chocolates. You never know what you’re gonna get.", "context":"Forrest Gump", "source":"Forrest Gump",  "theme":"movies"},
+        {"quote":"Ray, next time someone asks you if you’re a god, you say YES!","context":"Ghostbusters","source":"Winston Zeddemore", "theme":"movies"},
+        {"quote":"They’ve done studies, you know. Sixty percent of the time, it works every time.","context":"Anchorman","source":"Brian Fantana", "theme":"movies"},
+        {"quote":"This is so bad it’s gone past good and back to bad again.","context":"Ghost World","source":"Enid", "theme":"movies"},
+        {"quote":"Go that way, really fast. If something gets in your way, turn.","context":"Better Off Dead","source":"Charles De Mar", "theme":"movies"},
+        {"quote":"Put. That coffee. Down. Coffee's for closers only.","context":"Glengarry Glen Ross","source":"Blake", "theme":"movies"},
+        {"quote":"Good? Bad? I'm the guy with the gun.","context":"Army of Darkness","source":"Ash", "theme":"movies"},
+        {"quote":"Are you gonna bark all day, little doggy, or are you gonna bite?","context":"Reservoir Dogs","source":"Mr. Blonde", "theme":"movies"},
+        {"quote":"There are two types of people in the world - those with a gun, and those who dig. Now dig!","context":"The Good, the Bad, and the Ugly","source":"Blondie", "theme":"movies"},
+        {"quote":"If you try to run, I've got six little friends and they can all run faster than you can.","context":"From Dusk Til Dawn","source":"Seth", "theme":"movies"},
+        {"quote":"Are you gonna do something? Or just stand there and bleed?","context":"Tombstone","source":"Wyatt Earp", "theme":"movies"},
+        {"quote":"I love the smell of napalm in the morning","context":"Apocalypse now","source":"Kilgore", "theme":"movies"},
+        {"quote":"I could be the walrus. I'd still have to bum rides off people","context":"Ferris Bueller's Day Off","source":"Ferris", "theme":"movies"},
+        {"quote":"You either die a hero, or live long enough to see yourself become the villain.","context":"The Dark Knight","source":"Harvey Dent", "theme":"movies"},
+        {"quote":"If you hold back anything, I'll kill ya. If you bend the truth or I think you're bending the truth, I'll kill ya. If you forget anything, I'll kill ya. In fact, you're gonna have to work very hard to stay alive, Nick. Now, do you understand everything I've just said? 'Cause if you don't, I'll kill ya! Now, Mr Bubble and Squeak, you may enlighten me.","context":"Lock, Stock and Two Smoking Barrels","source":"Rory Breaker", "theme":"movies"},
+        {"quote":"You should never underestimate the predictability of stupidity.","context":"Snatch","source":"Bullet Tooth Tony", "theme":"movies"},
+        {"quote":"In the quiet words of the Virgin Mary... come again? ","context":"Snatch","source":"Brick Top", "theme":"movies"},
+        {"quote":"Heavy is good, heavy is reliable. If it doesn't work you can always hit them with it. ","context":"Snatch","source":"Boris 'The Blade' Yurinov", "theme":"movies"},
+        {"quote":"Oh, I'm sorry, did I break your concentration? ","context":"Pulp Fiction","source":"Jules", "theme":"movies"},
+        {"quote": "Momentum, a function of mass and velocity, is conserved between portals. In layman's terms, speedy thing goes in, speedy thing comes out.","context":"Portal","source":"GlaDOS", "theme":"games"},
+        {"quote": "You talkin' to me?","context": "Taxi Driver","source": "Travis Bickle", "theme": "movies"},
+        {"quote": "Are you not entertained! Are you not entertained! Is this not why you are here!","context": "Gladiator","source": "Maximus", "theme": "movies"},
+        {"quote": "Here's Johnny","context": "Jack Torrance","source": "The Shining", "theme": "movies"},
+        {"quote": "Mama always said life was like a box of chocolates. You never know what you're gonna get.","context": "Forest Gump","source": "Forest Gump", "theme": "movies"},
+        {"quote": "E.T. phone home","context": "E.T.","source": "E.T. The Extra-Terrestrial", "theme": "movies"},
+        {"quote": "Toto, I've got a feeling we're not in Kansas anymore","context": "Dorothy","source": "The Wizard of Oz", "theme": "movies"},
+        {"quote":"Open the pod bay doors, please, HAL","context":"2001: A Space Odyssey","source":"Dave", "theme":"movies"},
+        {"quote":"I'm not bad. I'm just drawn that way.", "source":"Jessica Rabbit", "context":"Who Framed Roger Rabbit",  "theme":"movies"},
+        {"quote":"When I first saw you, I thought you were handsome. Then, of course, you spoke.", "source":"Carol", "context":"As Good As It Gets",  "theme":"movies"},
+        {"quote":"Listen, here's the thing. If you can't spot the sucker in your first half hour at the table, then you ARE the sucker.", "source":"Mike", "context":"Rounders",  "theme":"movies"},
+        {"quote":"The cake is a lie","context":"Portal","source":"Corpse", "theme":"games"},
+        {"quote":"Welcome, everybody, to Monday Night Combat! We'd like to remind fans that we're committed to creating a safe, comfortable, enjoyable experience for only our Club-level and VIP guests. Everybody else...pay up for better seats next time!", "context":"Monday Night Combat", "source": "Mickey",  "theme":"games"},
+        {"quote":"Good men mean well. We just don't always end up doing well", "context":"Dead Space 3", "source": "Isaac",  "theme":"games"},
+        {"quote":"The next time you raise a hand to me will be the last time you have hands", "context":"Game of Thrones", "source":"Khaleesi", "theme":"movies"},
+        {"quote":"I'm not in the meth business. I'm in the empire business", "context":"Breaking Bad", "source":"Walter", "theme":"movies"},
+        {"quote":"If you don't like what's being said, change the conversation", "context":"Mad Men", "source":"Don Draper", "theme":"movies"}
       ],
       startingQuote: 0, 
       maxQuotes: 15,
       endQuote: 15,
-      themeFilter: ""
+      quoteFilter: "",
+      selectedTheme: "All"
+
     };
   },
 
   computed: {
-    totalQuotes: function() {
-      return this.quotes.slice(this.startingQuote, this.endQuote)
-    },
+
     numOfPages: function() {
       return Math.ceil(this.quotes.length / this.maxQuotes)
+      
     },
+
+    splicedQuotes: function() {
+      return this.filteredQuotes.slice(this.startingQuote, this.endQuote)
+    },
+
+    filteredQuotes: function() {
+      console.log(this.selectedTheme)
+      var vm = this;
+      var theme = vm.selectedTheme;
+      
+      if(theme === "All") {
+        return vm.quotes;
+      } else {
+        return vm.quotes.filter(function(quote) {
+          return quote.theme === theme;
+        });
+      }
+    }
   },
-  
-  
-  // created: function() {
-  //   const agent = new https.Agent({  
-  //     rejectUnauthorized: false
-  //   });
-  //   axios.get('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json', { httpsAgent: agent })
-  //   .then(response => (this.info = response))
-  //   },
 
   methods: {
     changePage: function (page) {
       this.startingQuote = (page - 1 ) * this.maxQuotes;
       this.endQuote = page * this.maxQuotes;
-      // console.log("We are here")
     }
-    // nextPage: function () {  
-    //   this.startingQuote += this.maxQuotes;
-    //   this.endQuote += this.maxQuotes;
-    //   if(this.startingQuote >= this.quotes.length){
-    //     this.startingQuote = 0;
-    //     this.endQuote = this.maxQuotes
-    //   }
-    //   console.log(this.filteredQuotes)
-    // },
-    // previousPage: function () {
-    //   this.startingQuote -= this.maxQuotes;
-    //   this.endQuote -= this.maxQuotes;
-    //   if(this.startingQuote < 0){
-    //     this.endQuote = this.quotes.length
-    //     this.startingQuote = this.quotes.length - this.maxQuotes
-    //   }
-    // }
+    
+
 
 
   }
